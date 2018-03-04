3bcbebd58e7d168b763cfb12b433f9c4e99bf6e1
Franco <fz.sinche.s@gmail.com>
2018-03-04 16:03:27 -0500

    video 5:  Controladores - reforzar el video 4 de Pruebas

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return 'Home';
 });
 
-Route::get('/usuarios',function(){
-  return 'Usuarios';
-});
+Route::get('/usuarios','UserController@index');
 
-Route::get('/usuarios/nuevo', function() {
-  return "Creando nuevo usuario";
-});
+Route::get('/usuarios/{id}', 'UserController@show')
+  ->where('id','[0-9]+');
 
-Route::get('/usuarios/{id}', function($id) {
-  return "Mostrando el detalle del usuario: {$id}";
-});
+Route::get('/usuarios/nuevo', 'UserController@create');
