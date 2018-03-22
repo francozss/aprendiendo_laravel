54ec683ee454fb64a8dfcb59c16fb08f44671d40
Franco Z <fz.sinche.s@gmail.com>
2018-03-22 16:33:01 -0500

    Commit de prueba

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,7 +1,9 @@
 @extends('template')
 @section('title', "Usuario {$user->id}")
 @section('content')
+
   <h1>Usuario # {{ $user->id }}</h1>
+
   <p>Nombre del usuario: {{ $user->name }}</p>
   <p>Correo del usuario: {{ $user->email }}</p>
   <p>
