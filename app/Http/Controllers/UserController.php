4a3e276115fc3a4be5045819732ed49a58fa0871
Franco Z <fz.sinche.s@gmail.com>
2018-05-05 08:22:24 -0500

    Commit de correcion hasta el video 30

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -31,7 +31,9 @@ class UserController extends Controller
     }
     public function store(){
       $data = request()->validate([
-        'name' => 'required'
+        'name' => 'required',
+				'email' =>  ['required', 'email', 'unique:users,email'],
+				'password' => 'required',
       ], [
         'name.required' => 'El campo nombre es obligatorio'
       ]);
